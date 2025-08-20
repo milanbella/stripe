b72b93044f66804cf2743d0c7024b72c9cd3f6c1
Milan Bella <milan.bella>
2025-08-20 07:58:55 +0200

    m

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+rm -rf temp_deploy
+mkdir temp_deploy
+cp -rf dist temp_deploy/html_paygate
+cd temp_deploy
+	tare -cvzf html_paygate.tar.gz html_paygate
+sftp -b - root@usrv <<EOF
+cd /var/www
+put html_paygate.tar.gz
+bye
+EOF
+ssh root@usrv  'rm -rf /var/www/html_paygate'
+ssh root@usrv  'cd /var/www && tar xvf html_paygate.tar.gz &&  rm html_paygate.tar.gz'
+ssh root@usrv  'chown -R www-data:www-data /var/www/html_paygate'
+cd ..
+rm -rf temp_deploy
